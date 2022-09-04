2cc5924038407e64fabef78beed82439afe5a610
Ronald McIntyre <ronxcamacho@gmail.com>
2022-09-04 15:47:58 -0400

    cleared issue with submission

--- api/routes/reset/reset-router.js
+++ api/routes/reset/reset-router.js
@@ -20,8 +20,7 @@ server.get('/', (req,res) => {
 
 
 server.post('/add', (req,res) => {
-    console.log(req)
-    resetDB.submitReset(req.body.uuid)
+    resetDB.submitReset(req.body)
     .then( reset => {
         res.status(200).json('success')
     })
